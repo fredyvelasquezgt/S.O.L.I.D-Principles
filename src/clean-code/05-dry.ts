1daab0f38a02acf795e4b18b8d509b94f5312ef5
Fredy Velasquez <macacoloco696@gmail.com>
2022-08-16 17:19:28 -0600

    DRY principle applied for verifications

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,12 +7,28 @@ class Product {
         public size: Size = '',
     ){}
 
-        toString() {
-            //No Dry
-            if(this.name.length <= 0) throw Error('name is empty')
-            if(this.price <= 0) throw Error('price is zero')
-            if(this.size.length <= 0) throw Error('size is empty')
 
+            isProductReady ():boolean {
+                for (const key in this) {
+                    switch(typeof this[key]) {
+                        case 'string':
+                            if(( <string> <unknown> this[key]).length <= 0 ) throw Error(`${key} is empty`)
+                        break;
+                        case 'number':
+                            if(( <number> <unknown> this[key]) <= 0 ) throw Error(`${key} is cero`)
+                        break;
+                        default:
+                            throw Error(`${typeof this[key]} is not valid`)
+    
+                    }
+                }
+
+                return true;
+            }
+
+        toString() {
+           
+           if(!this.isProductReady) return;
 
             return `${this.name} (${this.price}, ${this.size})`
         }
@@ -21,6 +37,6 @@ class Product {
 }
 
 (() => {
-    const bluePants = new Product('blue large pants', 10, 'M');
+    const bluePants = new Product('Blue pants');
     console.log(bluePants.toString())
 })();
