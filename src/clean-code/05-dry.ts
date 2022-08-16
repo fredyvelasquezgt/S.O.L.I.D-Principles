5c45fc63bc744eb96aac4e8e632de2119f63563a
Fredy Velasquez <macacoloco696@gmail.com>
2022-08-16 16:54:57 -0600

    no DRY principle applied

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -1,10 +1,26 @@
+type Size = '' | 'S' | 'M' | 'XL';
+
 class Product {
     constructor(
-        public name: string,
+        public name: string='',
+        public price: number = 0,
+        public size: Size = '',
     ){}
+
+        toString() {
+            //No Dry
+            if(this.name.length <= 0) throw Error('name is empty')
+            if(this.price <= 0) throw Error('price is zero')
+            if(this.size.length <= 0) throw Error('size is empty')
+
+
+            return `${this.name} (${this.price}, ${this.size})`
+        }
+
+
 }
 
 (() => {
-    const bluePants = new Product('Blue large pants');
-    console.log(bluePants)
+    const bluePants = new Product('blue large pants', 10, 'M');
+    console.log(bluePants.toString())
 })();
