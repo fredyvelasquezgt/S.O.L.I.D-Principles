556cf0c33374c396c99d86eebe5fe5ad1472d535
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-07 16:35:49 -0600

    classes finished

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,20 +1,72 @@
 (() => {
 
+    //No aplicando el principio de responabilidad unica
+
+
+
+
     type Gender = 'M'|'F';
 
     class Person {
-        public name: string;
-        public gender: Gender;
-        public birthdate: Date;
+       
         
-        constructor(name:string, gender: Gender, birthdate: Date) {
-            this.name = name;
-            this.gender = gender;
-            this.birthdate = birthdate;
+        constructor(
+            public name:string, 
+            public gender: Gender,
+            public birthdate: Date) {
+           
+        }
+    }
+
+    //El usuario tiene todas las caracteristicas de Person
+    class User extends Person {
+
+        public lastAccess: Date;
+
+        constructor (
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
         }
     }
 
 
-    const newPerson = new Person('Fredy', 'M', new Date('2002-05-30'))
-    console.log({newPerson})
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usrr/home',
+        '/home',
+        'fredy@google.com',
+        'Admin',
+        'Fernando',
+        'M',
+        new Date('2002-05-30')
+    );
+
+    console.log({userSettings})
+
+
+
+
 })();
