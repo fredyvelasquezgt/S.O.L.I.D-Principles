474745a527db82e7d9b3fc3fd4b9190f5c9edddd
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-08 21:07:28 -0600

    classes finished

---
+++ src/clean-code/06-classes-b.ts
@@ -0,0 +1,76 @@
+(() => {
+
+    //No aplicando el principio de responabilidad unica
+
+
+    type Gender = 'M'|'F';
+
+    interface PersonProps {
+        public name:string, 
+        public gender: Gender,
+        public birthdate: Date
+    }
+
+
+    class Person {
+       
+        
+        constructor(
+           
+            ) {
+           
+        }
+    }
+
+    //El usuario tiene todas las caracteristicas de Person
+    class User extends Person {
+
+        public lastAccess: Date;
+
+        constructor (
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ) {
+            super(name, gender, birthdate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true;
+        }
+    }
+
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+            super(email, role, name, gender, birthdate);
+        }
+    }
+
+    const userSettings = new UserSettings(
+        '/usrr/home',
+        '/home',
+        'fredy@google.com',
+        'Admin',
+        'Fernando',
+        'M',
+        new Date('2002-05-30')
+    );
+
+    console.log({userSettings})
+
+
+
+
+})();
