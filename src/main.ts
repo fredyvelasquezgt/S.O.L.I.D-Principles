e0dea3fb88cdf9121453d299f23e18094ade0ad3
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-12 12:42:44 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.css'
 // import './clean-code/05-dry'
-import './clean-code/07-tarea'
+// import './clean-code/07-tarea'
+import './code-smells/02-low-coupling';
+
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
