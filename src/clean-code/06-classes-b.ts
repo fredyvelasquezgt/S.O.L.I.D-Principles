660799c9ba416a5c02325b9a66f1bc3bbfc80e41
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-11 22:21:40 -0600

    clase refactorizada

--- src/clean-code/06-classes-b.ts
+++ src/clean-code/06-classes-b.ts
@@ -5,37 +5,52 @@
 
     type Gender = 'M'|'F';
 
+    //Crear interfaz
     interface PersonProps {
-        public name:string, 
-        public gender: Gender,
-        public birthdate: Date
+         name:string;
+         gender: Gender;
+         birthdate: Date;
     }
 
-
     class Person {
-       
-        
-        constructor(
-           
-            ) {
-           
+
+        public birthdate: Date;
+        public name: string;
+        public gender: Gender;
+
+        constructor({name,gender,birthdate} : PersonProps){
+           this.name = name;
+           this.gender = gender;
+           this.birthdate = birthdate;
         }
+    }
+
+    interface UserProps {
+        email: string;
+        role: string;
+        name: string;
+        gender: Gender;
+        birthdate: Date;
     }
 
     //El usuario tiene todas las caracteristicas de Person
     class User extends Person {
 
         public lastAccess: Date;
+        public email: string;
+        public role: string;
 
         constructor (
-            public email: string,
-            public role: string,
-            name: string,
-            gender: Gender,
-            birthdate: Date
-        ) {
-            super(name, gender, birthdate);
+            {email,
+            role,
+            name,
+            gender,
+            birthdate,}
+        ):UserProps {
+            super({name, gender, birthdate});
             this.lastAccess = new Date();
+            this.email = email;
+            this.role = role;
         }
 
         checkCredentials() {
@@ -44,28 +59,44 @@
     }
 
 
+    interface UserSettingsProps {
+        workingDirectory: string;
+        lastOpenFolder: string;
+        email: string;
+        role: string;
+        name: string;
+        gender: Gender;
+        birthdate: Date;
+    }
+
     class UserSettings extends User {
+        
+        public workingDirectory: string;
+        public lastOpenFolder: string;
+
         constructor(
-            public workingDirectory: string,
-            public lastOpenFolder: string,
-            email: string,
-            role: string,
-            name: string,
-            gender: Gender,
-            birthdate: Date
+           { workingDirectory,
+            lastOpenFolder,
+            email,
+            role,
+            name,
+            gender,
+            birthdate} : UserSettingsProps
         ){
-            super(email, role, name, gender, birthdate);
+            super({email, role, name, gender, birthdate});
+            this.workingDirectory = workingDirectory;
+            this.lastOpenFolder = lastOpenFolder;
         }
     }
 
     const userSettings = new UserSettings(
-        '/usrr/home',
-        '/home',
-        'fredy@google.com',
-        'Admin',
-        'Fernando',
-        'M',
-        new Date('2002-05-30')
+       { workingDirectory: '/usrr/home',
+       lastOpenFolder: '/home',
+        email: 'fredy@google.com',
+       role: 'Admin',
+       name: 'Fernando',
+       gender: 'M',
+       birthdate: new Date('2002-05-30')}
     );
 
     console.log({userSettings})
