5e646de6778649b490a99d52c4eebd0afff4ea55
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-14 16:39:47 -0600

    Segregation principle created

---
+++ src/solid/04-segregation.ts
@@ -0,0 +1,44 @@
+interface Bird {
+    eat(): void;
+}
+
+interface FlyingBird {
+    fly(): void;
+}
+
+interface RunningBird {
+    run(): void;
+}
+
+interface SwimmerBird {
+    swim(): void;
+}
+
+
+class Tucan implements Bird, FlyingBird {
+
+    public fly() {return 100;}
+    public eat() {}
+
+}
+
+class Humminbird implements Bird, FlyingBird {
+    
+    public fly() {return 200;}
+    public eat() {}
+
+}
+
+class Ostrich implements Bird, RunningBird {
+    
+    public eat() {}
+    public run() {}
+
+}
+
+
+class Penguin implements Bird, SwimmerBird {
+    
+    public eat() {}
+    public swim() {}
+}
