9d284a5de005ec5128888def8f96eb2de46a5766
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-13 22:10:12 -0600

    SRP totally applied

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -38,28 +38,33 @@
     // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
     // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
     class ProductBloc {
+
+        private productService: ProductService;
+        private mailer: Mailer;
+
+        //Al hacer esto sigo manteniendo la responabilidad unica que tiene el product y el service
+        constructor(productService: ProductService, mailer: Mailer) {
+            this.productService = productService;
+            this.mailer = mailer;
+        }
     
         loadProduct( id: number ) {
-            // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
-
+            this.productService.getProduct(id);
         }
     
         saveProduct( product: Product ) {
-            // Realiza una petición para salvar en base de datos 
+            this.productService.saveProduct(product)
         }
     
         notifyClients() {
-            console.log('Enviando correo a los clientes');
+            this.mailer.sendEmail(['fredyy@google.com'], 'to-client' )
         }
     
     }
     
     class CartBloc {
 
-
         private itemsInCart: Object [] = [];
-
 
         addToCart(productId: number){
             console.log('agregando al carrito...', productId)
@@ -67,11 +72,13 @@
         }
     }
 
+    const productService = new ProductService();
+    const mailer = new Mailer();
 
-
-    const productBloc = new ProductBloc();
+    const productBloc = new ProductBloc(productService, mailer);
     const cartBloc = new CartBloc();
 
+    //Con la refactorizacion cambia pero sigue haciendo lo mismo
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
