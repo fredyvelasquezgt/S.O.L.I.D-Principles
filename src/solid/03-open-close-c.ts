150e6d3276372b4bf8daa6e3d56b1116db586565
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-14 00:56:08 -0600

    axios removed and fetch added

--- src/solid/03-open-close-c.ts
+++ src/solid/03-open-close-c.ts
@@ -3,8 +3,20 @@ import axios from 'axios';
 
 export class HttpClient {
 
+    // async get (url: string) {
+    //     const { data, status } = await axios.get(url);
+    //     return {data, status};
+    // }
+
+    //Esta es una nueva implementacion, quitando axios
     async get (url: string) {
-        const { data, status } = await axios.get(url);
-        return {data, status};
+
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        return {data,status: resp.status}
     }
+
+
+
 }
