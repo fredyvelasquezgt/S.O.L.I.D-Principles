31da70f18623f86d0ede324e2d2b7e003ef4be98
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-14 15:19:30 -0600

    liskov classes added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 // import './clean-code/05-dry'
 // import './clean-code/07-tarea'
-import './solid/02-open-close-a';
+import './solid/03-liskov-a'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
