e27af358f617642f151d7a41439e8cab5f68ce41
Fredy Velasquez <macacoloco696@gmail.com>
2022-09-14 15:29:54 -0600

    abstract class created

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,39 +1,72 @@
 
+//sirven para trabajar con herencia
+export abstract class Vehicle {
 
-export class Tesla {
+    // getNumberOfSeats(): number {
+    //     throw Error('method not implemented');
+    // }
 
-    constructor( private numberOfSeats: number ) {}
+    abstract getNumberOfSeats():number;
 
-    getNumberOfTeslaSeats() {
+}
+
+
+export class Tesla extends Vehicle {
+    
+  
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+
+}
+
+export class Audi extends Vehicle {
+   
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Audi {
+export class Toyota extends Vehicle {
+   
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfAudiSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Toyota {
+export class Honda extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfToyotaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Honda {
+export class Volvo extends Vehicle {
+    constructor(private numberOfSeats: number) {
+        super();
+    }
 
-    constructor( private numberOfSeats: number ) {}
-
-    getNumberOfHondaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
+
 }
-
-
